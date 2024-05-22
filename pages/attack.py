987d1165c3b25d8e1cefa6475a9bf5c1ed3e2e37
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 12:52:59 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -4,23 +4,24 @@ import time
 import sys
 import os
 
+# Adding the parent directory to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def main_attack():
+def main():
+    # Get query parameters to determine the page
     query_params = st.experimental_get_query_params()
     page = query_params.get("page", ["main"])[0]
 
-       if page == "anxiety_attack_protocol":
-        import anxiety_attack_protocol as attack_protocol
+    if page == "anxiety_attack_protocol":
+        from pages import anxiety_attack_protocol as attack_protocol
         attack_protocol.show()
     elif page == "anxiety_protocol":
-        import anxiety_protocol
+        from pages import anxiety_protocol
         anxiety_protocol.show()
-        
     else:
-        show()
+        show_main_page()
 
-def show():
+def show_main_page():
     st.image("Logo.jpeg", width=600)
     st.write("---")
 
@@ -28,19 +29,19 @@ def show():
 
     answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
     if answer == "Yes":
-        st.switch_page("pages/anxiety_attack_protocol.py")
+        switch_pages("anxiety_attack_protocol")
     else:
         answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
         if answer_2 == "Yes":
-            st.switch_page("pages/anxiety_protocol.py")
+            switch_pages("anxiety_protocol")
         else:
             st.write("Reassess your feelings.")
 
-def switch_page(page_name):
-    st.success("Redirecting to {} page...".format(page_name))
-    time.sleep(3)
+def switch_pages(page_name):
+    st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
+    time.sleep(2)
     st.experimental_set_query_params(page=page_name)
     st.experimental_rerun()
 
 if __name__ == "__main__":
-    main_attack() 
+    main()
