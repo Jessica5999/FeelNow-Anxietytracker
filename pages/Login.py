478f793b45b1665d8f7cc83981859f5456980e43
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-22 10:12:46 +0200

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -1,8 +1,8 @@
 import binascii
 import streamlit as st
 import pandas as pd
-from github_contents import GithubContents
 import bcrypt
+from github_contents import GithubContents
 
 # Constants
 DATA_FILE = "MyLoginTable.csv"
