a1ceb924cd529a4efafa9220d6274e1e02591d43
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 12:45:57 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -1,34 +1,46 @@
 import streamlit as st
 from PIL import Image
-from pages import Anxiety_Attack_Protocol, Anxiety_protocol
+import time
+import sys
+import os
+
+# Adding the parent directory to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def main_attack():
+    query_params = st.experimental_get_query_params()
+    page = query_params.get("page", ["main"])[0]
+
+    if page == "anxiety_attack_protocol":
+        from pages import anxiety_attack_protocol as attack_protocol
+       
+    elif page == "anxiety_protocol":
+        from pages import anxiety_protocol
+        
+    else:
+        show()
 
 def show():
     st.image("Logo.jpeg", width=600)
     st.write("---")
-    
+
     st.write("Anxiety Assessment:")
-    
-    st.write("Do you feel like you're having an Anxiety Attack right now?")
-    if st.button("Yes"):
-        anxiety_attack_protocol()
-    elif st.button("No"):
-        redirect_question_2()
-        if st.button("Yes "):
-            anxiety_protocol()
-        elif st.button("No "):
-            No_2_question()
 
-def anxiety_attack_protocol():
-    st.write("Redirecting to Anxiety Attack Protocol page...")
+    answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
+    if answer == "Yes":
+        st.switch_pages("pages/anxiety_attack_protocol.py")
+    else:
+        answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
+        if answer_2 == "Yes":
+            st.switch_pages("pages/anxiety_protocol.py")
+        else:
+            st.write("Reassess your feelings.")
 
-def redirect_question_2():
-    st.write("Are you anxious right now?")
-
-def anxiety_protocol():
-    st.write("Redirecting to Anxiety Protocol page...")
-
-def No_2_question():
+def switch_pages(page_name):
+    st.success("Redirecting to {} page...".format(page_name))
+    time.sleep(3)
+    st.experimental_set_query_params(page=page_name)
     st.experimental_rerun()
 
 if __name__ == "__main__":
-    main()
+    main_attack() 
