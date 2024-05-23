3ad8a11177fd1e025c451467c93fc76a43ef0ebe
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-23 10:02:32 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -113,6 +113,14 @@ def init_github():
             st.secrets["github"]["token"])
         print("github initialized")
 
+def init_credentials():
+    """Initialize or load the dataframe."""
+    if 'df_users' not in st.session_state:
+        if st.session_state.github.file_exists(DATA_FILE):
+            st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
+        else:
+            st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
+
 def main():
     init_github()
     init_credentials()
