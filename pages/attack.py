a2652e43e68606fce04db6794df4a0b88f799967
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-23 08:40:22 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -12,34 +12,29 @@ def main():
 
     if page == "anxiety_protocol":
         from pages import anxiety_protocol
-        
+        anxiety_protocol.show()
     elif page == "anxiety_attack_protocol":
         from pages import anxiety_attack_protocol as attack_protocol
-        
+        attack_protocol.show()
     else:
         show_main_page()
 
 def show_main_page():
     st.image("Logo.jpeg", width=600)
     st.write("---")
-
     st.write("Anxiety Assessment:")
 
     answer = st.radio("Are you anxious right now, without having an attack?", ("Yes", "No"))
     if answer == "Yes":
-        st.switch_page("pages/anxiety_protocol.py")
+        st.experimental_set_query_params(page="anxiety_protocol")
+        st.experimental_rerun()
     else:
         answer_2 = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
         if answer_2 == "Yes":
-            st.switch_page("pages/attack_protocol.py")
+            st.experimental_set_query_params(page="anxiety_attack_protocol")
+            st.experimental_rerun()
         else:
             st.write("Reassess your feelings")
 
-def switch_page(page_name):
-    st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
-    time.sleep(3)
-    st.experimental_set_query_params(page=page_name)
-    st.experimental_rerun()
-
 if __name__ == "__main__":
-    show_main_page()
+    main()
