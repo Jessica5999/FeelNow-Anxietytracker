9bde1696258bc82f35c2c3050871581a42924c3c
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-25 11:49:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,20 +20,13 @@ def main_page():
     st.image("Logo.jpeg", width=600)
     st.subheader("Anxiety Tracker Journal")
 
-    # Create three columns with the first two taking up most of the space and the last one for the selectbox
-    col1, col2, col3 = st.columns([0.8, 0.1, 0.1])
-    
-    with col3:
-        languages = {
-            "English": "en",
-            "German": "de",
-            "Spanish": "es",
-            "French": "fr",
-            "Chinese": "zh-cn"
-        }
-
-        selected_language = st.selectbox("Select Language", list(languages.keys()))
-        target_language = languages[selected_language]
+    languages = {
+        "English": "en",
+        "German": "de",
+        "Spanish": "es",
+        "French": "fr",
+        "Chinese": "zh-cn"
+    }
 
     original_text = """
         Welcome to FeelNow, your anxiety attack journal.
@@ -50,13 +43,34 @@ def main_page():
         You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
     """
 
+    # Create a placeholder for the language selection button
+    language_placeholder = st.empty()
+
+    # Translate the text
+    selected_language = language_placeholder.selectbox("Select Language", list(languages.keys()), key="language_select")
+    target_language = languages[selected_language]
     translated_text = translate_text(original_text, target_language)
+    
     st.write(translated_text)
 
     col1, col2 = st.columns([0.8, 0.2])
     with col2:
         if st.button("Login/Register"):
             st.switch_page("pages/login.py")
+
+    # Use CSS to position the language selection button
+    st.markdown(
+        """
+        <style>
+        .stSelectbox {
+            position: fixed;
+            bottom: 20px;
+            left: 20px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 def switch_page(page_name):
     st.success("Redirecting to {} page...".format(page_name))
