35b96cfc782132478215e9e69bef390f036dc7c1
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-28 18:47:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,23 +39,23 @@ def main_page():
 
     # Original text
     original_text = """
-        Welcome to FeelNow, your anxiety attack journal.
-        This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
-        and feelings during anxiety attacks.
-        
-        ## What is FeelNow
-        FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
-        
-        ## What can the App do
-        The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies taking notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
-        
-        ## How do I use it
-        You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
+Welcome to FeelNow, your anxiety attack journal.
+This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
+and feelings during anxiety attacks.
+
+## What is FeelNow
+FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
+
+## What can the App do
+The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies taking notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
+
+## How do I use it
+You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
     """
 
     # Translate the text
     translated_text = translate_text(original_text, target_language)
-    st.markdown(translated_text, unsafe_allow_html=True)  # Render the translated text as raw HTML
+    st.write(translated_text)
 
     # Login/Register button
     col1, col2 = st.columns([0.8, 0.2])
