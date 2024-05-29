6a69b6b447aa5daa11a51c27b6ee93b69b74e6ca
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 13:32:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main_page():
         "Spanish": "es",
         "French": "fr",
         "Italian": "it"
-        "Portuguese": "pt"
+        "Portuguese": "por"
     }
 
     # Language selection
