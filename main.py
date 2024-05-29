a212f6a9702dfd4db5e968af2d8bea7fc3ad8710
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 10:08:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import time
 from github_contents import GithubContents
 from deep_translator import GoogleTranslator  # Import the GoogleTranslator class from the deep_translator library
 
-# Initialize GithubContents with secrets from Streamlit
 github = GithubContents(
     st.secrets["github"]["owner"],
     st.secrets["github"]["repo"],
@@ -32,9 +30,8 @@ def translate_text(text, target_language):
     return translation
 
 # Function to display the main page
-def main_page():
-    st.image("Logo.jpeg", width=600)
-    st.subheader("Anxiety Tracker Journal")
+def show():
+    st.title("Main Page")
 
     # Supported languages
     languages = {
@@ -45,35 +42,43 @@ def main_page():
         "Chinese": "zh-cn"
     }
 
-    original_text = """
-    Welcome to FeelNow, your anxiety attack journal.
-    This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
-    and feelings during anxiety attacks.
 
-    ## What is FeelNow
-    FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
+def main_page():
+    st.image("Logo.jpeg", width=600)
+    st.subheader("Anxiety Tracker Journal")
 
-    ## What can the App do
-    The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies taking notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
+     # Supported languages
+    languages = {
+        "English": "en",
+        "German": "de",
+        "Spanish": "es",
+        "French": "fr",
+        "Chinese": "zh-cn"
+    }
+orginal_text = """
+        Welcome to FeelNow, your anxiety attack journal.
+        This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
+        and feelings during anxiety attacks.
+        
+        ## What is FeelNow
+        FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
+        
+        ## What can the App do
+        The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies takeing notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
+        
+        ## How do I use it
+        You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
+        """
 
-    ## How do I use it
-    You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
-    """
+# Translate the text
+translated_text = translate_text(original_text, target_language)
+st.write(translated_text)
 
-    # Choose target language for translation
-    target_language = st.selectbox("Choose language", list(languages.keys()), index=0)
-    target_language_code = languages[target_language]
+col1, col2 = st.columns([0.8, 0.2])
+with col2:
+ if st.button("Login/Register"):
+    st.switch_page("pages/1_login.py")
 
-    # Translate the text
-    translated_text = translate_text(original_text, target_language_code)
-    st.write(translated_text)
-
-    col1, col2 = st.columns([0.8, 0.2])
-    with col2:
-        if st.button("Login/Register"):
-            switch_page("pages/1_login.py")
-
-# Function to switch pages
 def switch_page(page_name):
     st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
     time.sleep(3)
@@ -81,5 +86,4 @@ def switch_page(page_name):
     st.experimental_rerun()
 
 if __name__ == "__main__":
-    main_sidebar()
     main_page()
