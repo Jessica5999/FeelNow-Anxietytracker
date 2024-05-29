b1340faafcb58bf7f1e4d218393b1173d839c5e4
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 15:06:56 +0200

    Update 4_anxiety_attack_protocol.py

--- pages/4_anxiety_attack_protocol.py
+++ pages/4_anxiety_attack_protocol.py
@@ -1,166 +1,59 @@
 import streamlit as st
-import bcrypt
 import binascii
-import pytz
-import datetime
+import bcrypt
+import time
 import pandas as pd
 from github_contents import GithubContents
+from PIL import Image
+from deep_translator import GoogleTranslator
 
 # Constants
 DATA_FILE = "MyLoginTable.csv"
 DATA_COLUMNS = ['username', 'name', 'password']
 
-def main():
-    init_github()
-    init_credentials()
+def translate_text(text, target_language):
+    """Translate text using the deep_translator library."""
+    translator = GoogleTranslator(target=target_language)
+    translation = translator.translate(text)
+    return translation
 
-    if 'authentication' not in st.session_state:
-        st.session_state['authentication'] = False
-
-    if not st.session_state['authentication']:
-        options = st.sidebar.selectbox("Select a page", ["Login", "Register"])
-        if options == "Login":
-            login_page()
-        elif options == "Register":
-            register_page()
-    else:
-        st.sidebar.write(f"Logged in as {st.session_state['username']}")
-        anxiety_attack_protocol()
-
-        logout_button = st.sidebar.button("Logout")
-        if logout_button:
-            st.session_state['authentication'] = False
-            st.session_state.pop('username', None)
-            st.switch_page("main.py")
-            st.experimental_rerun()
-
-def anxiety_attack_protocol():
-    username = st.session_state['username']
-    data_file = f"{username}_data.csv"
+def main_page():
+    st.image("Logo.jpeg", width=600)
+    st.title(translate_text("Your Anxiety Tracker Journal", st.session_state['target_language']))
+    st.subheader(translate_text("Profile", st.session_state['target_language']))
     
-    if 'data' not in st.session_state:
-        if st.session_state.github.file_exists(data_file):
-            st.session_state.data = st.session_state.github.read_df(data_file)
+    if 'username' in st.session_state:
+        username = st.session_state['username']
+        
+        # Load user data
+        user_data = st.session_state.df_users.loc[st.session_state.df_users['username'] == username]
+        
+        if not user_data.empty:
+            st.write(translate_text("Username:", st.session_state['target_language']), username)
+            st.write(translate_text("Name:", st.session_state['target_language']), user_data['name'].iloc[0])
         else:
-            st.session_state.data = pd.DataFrame(columns=['Date', 'Time', 'Severity', 'Symptoms', 'Triggers', 'Help'])
+            st.error(translate_text("User data not found.", st.session_state['target_language']))
+    else:
+        st.error(translate_text("User not logged in.", st.session_state['target_language']))
+        if st.button(translate_text("Login/Register", st.session_state['target_language'])):
+            switch_page("pages/1_login.py")
 
-    st.title("Anxiety Attack Protocol")
+def anxiety_assessment():
+    st.subheader(translate_text("Anxiety Assessment:", st.session_state['target_language']))
+    st.write(translate_text("Do you feel like you're having an Anxiety Attack right now?", st.session_state['target_language']))
+    if st.button(translate_text("Yes", st.session_state['target_language'])):
+        switch_page("pages/4_anxiety_attack_protocol.py")
+    if st.button(translate_text("No", st.session_state['target_language'])):
+        anxiety_assessment2()
 
-    # Question 1: Date
-    date_selected = st.date_input("Date", value=datetime.date.today())
-
-    # Question 2: Time & Severity
-    add_time_severity()
-
-    # Question 3: Symptoms
-    st.subheader("Symptoms:")
-    col1, col2 = st.columns(2)
-    with col1:
-        symptoms_anxiety = st.checkbox("Anxiety")
-        symptoms_chestpain = st.checkbox("Chest Pain")
-        symptoms_chills = st.checkbox("Chills")
-        symptoms_chocking = st.checkbox("Chocking")
-        symptoms_cold = st.checkbox("Cold")
-        symptoms_coldhands = st.checkbox("Cold Hands")
-        symptoms_dizziness = st.checkbox("Dizziness")
-        symptoms_feelingdanger = st.checkbox("Feeling of danger")
-        symptoms_feelingdread = st.checkbox("Feeling of dread")
-        symptoms_heartracing = st.checkbox("Heart racing")
-        symptoms_hotflushes = st.checkbox("Hot flushes")
-        symptoms_irrationalthinking = st.checkbox("Irrational thinking")
-    with col2:
-        symptoms_nausea = st.checkbox("Nausea")
-        symptoms_nervous = st.checkbox("Nervousness")
-        symptoms_numbhands = st.checkbox("Numb Hands")
-        symptoms_numbness = st.checkbox("Numbness")
-        symptoms_palpitations = st.checkbox("Palpitations")
-        symptoms_shortbreath = st.checkbox("Shortness of Breath")
-        symptoms_sweating = st.checkbox("Sweating")
-        symptoms_tensemuscles = st.checkbox("Tense Muscles")
-        symptoms_tinglyhands = st.checkbox("Tingly Hands")
-        symptoms_trembling = st.checkbox("Trembling")
-        symptoms_tremor = st.checkbox("Tremor")
-        symptoms_weakness = st.checkbox("Weakness")
-    
-    if 'symptoms' not in st.session_state:
-        st.session_state.symptoms = []
-
-    new_symptom = st.text_input("Add new symptom:")
-    if st.button("Add Symptom") and new_symptom:
-        st.session_state.symptoms.append(new_symptom)
-
-    for symptom in st.session_state.symptoms:
-        st.write(symptom)
-
-    # Question 4: Triggers
-    st.subheader("Triggers:")
-    triggers = st.multiselect("Select Triggers", ["Stress", "Caffeine", "Lack of Sleep", "Social Event", "Reminder of traumatic event", "Alcohol", "Conflict", "Family problems"])
-    
-    if 'triggers' not in st.session_state:
-        st.session_state.triggers = []
-
-    new_trigger = st.text_input("Add new trigger:")
-    if st.button("Add Trigger") and new_trigger:
-        st.session_state.triggers.append(new_trigger)
-
-    for trigger in st.session_state.triggers:
-        st.write(trigger)
-
-    # Question 5: Did something Help against the attack?
-    st.subheader("Did something Help against the attack?")
-    help_response = st.text_area("Write your response here", height=100)
-
-    if st.button("Save Entry"):
-        new_entry = {
-            'Date': date_selected,
-            'Time': [entry['time'] for entry in st.session_state.time_severity_entries],
-            'Severity': [entry['severity'] for entry in st.session_state.time_severity_entries],
-            'Symptoms': st.session_state.symptoms,
-            'Triggers': triggers,
-            'Help': help_response
-        }
-        st.switch_page("pages/2_profile.py")
-        # Create a DataFrame from the new entry
-        new_entry_df = pd.DataFrame([new_entry])
-        
-        # Append the new entry to the existing data DataFrame
-        st.session_state.data = pd.concat([st.session_state.data, new_entry_df], ignore_index=True)
-        
-        # Save the updated DataFrame to the user's specific CSV file on GitHub
-        st.session_state.github.write_df(data_file, st.session_state.data, "added new entry")
-        st.success("Entry saved successfully!")
-
-        # Clear the severity entries after saving
-        st.session_state.time_severity_entries = []
-
-    # Display saved entries
-    st.subheader("Saved Entries")
-    st.write(st.session_state.data)
-
-def add_time_severity():
-    if 'time_severity_entries' not in st.session_state:
-        st.session_state.time_severity_entries = []
-
-    st.subheader("Time & Severity")
-
-    # Display the current time
-    current_time = datetime.datetime.now(pytz.timezone('Europe/Zurich')).strftime('%H:%M')
-    st.write(f"Current Time: {current_time}")
-
-    # Button to add a new time-severity entry
-    with st.form(key='severity_form'):
-        severity = st.slider("Severity (1-10)", min_value=1, max_value=10, key=f"severity_slider")
-        if st.form_submit_button("Add Severity"):
-            new_entry = {
-                'time': current_time,
-                'severity': severity
-            }
-            st.session_state.time_severity_entries.append(new_entry)
-            st.success(f"Added entry: Time: {current_time}, Severity: {severity}")
-
-    # Display all time-severity entries
-    for entry in st.session_state.time_severity_entries:
-        st.write(f"Time: {entry['time']}, Severity: {entry['severity']}")
+def anxiety_assessment2():
+    st.write(translate_text("Are you anxious right now?", st.session_state['target_language']))
+    if st.button(translate_text("Yes", st.session_state['target_language'])):
+        switch_page("pages/5_anxiety_protocol.py")
+    elif st.button(translate_text("No", st.session_state['target_language'])):
+        gif_url = "https://64.media.tumblr.com/28fad0005f6861c08f2c07697ff74aa4/tumblr_n4y0patw7Q1rn953bo1_500.gif"
+        gif_html = f'<img src="{gif_url}" width="400" height="300">'
+        st.markdown(gif_html, unsafe_allow_html=True)
 
 def init_github():
     """Initialize the GithubContents object."""
@@ -180,68 +73,97 @@ def init_credentials():
             st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
 
 def login_page():
-    """Login an existing user."""
-    st.title("Login")
+    """ Login an existing user. """
+    st.title(translate_text("Login", st.session_state['target_language']))
     with st.form(key='login_form'):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        if st.form_submit_button("Login"):
+        username = st.text_input(translate_text("Username", st.session_state['target_language']))
+        password = st.text_input(translate_text("Password", st.session_state['target_language']), type="password")
+        if st.form_submit_button(translate_text("Login", st.session_state['target_language'])):
             authenticate(username, password)
+            if st.session_state['authentication']:
+                switch_page("pages/2_profile.py")
 
 def register_page():
-    """Register a new user."""
-    st.title("Register")
+    """ Register a new user. """
+    st.title(translate_text("Register", st.session_state['target_language']))
     with st.form(key='register_form'):
-        new_username = st.text_input("New Username")
-        new_name = st.text_input("Name")
-        new_password = st.text_input("New Password", type="password")
-        if st.form_submit_button("Register"):
-            hashed_password = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())  # Hash the password
-            hashed_password_hex = binascii.hexlify(hashed_password).decode()  # Convert hash to hexadecimal string
+        new_username = st.text_input(translate_text("New Username", st.session_state['target_language']))
+        new_name = st.text_input(translate_text("Name", st.session_state['target_language']))
+        new_password = st.text_input(translate_text("New Password", st.session_state['target_language']), type="password")
+        if st.form_submit_button(translate_text("Register", st.session_state['target_language'])):
+            hashed_password = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())
+            hashed_password_hex = binascii.hexlify(hashed_password).decode()
             
-            # Check if the username already exists
             if new_username in st.session_state.df_users['username'].values:
-                st.error("Username already exists. Please choose a different one.")
-                return
+                st.error(translate_text("Username already exists. Please choose a different one.", st.session_state['target_language']))
             else:
                 new_user = pd.DataFrame([[new_username, new_name, hashed_password_hex]], columns=DATA_COLUMNS)
                 st.session_state.df_users = pd.concat([st.session_state.df_users, new_user], ignore_index=True)
                 
-                # Writes the updated dataframe to GitHub data repository
                 st.session_state.github.write_df(DATA_FILE, st.session_state.df_users, "added new user")
-                st.success("Registration successful! You can now log in.")
+                st.success(translate_text("Registration successful! You can now log in.", st.session_state['target_language']))
 
 def authenticate(username, password):
-    """
-    Authenticate the user.
-
-    Parameters:
-    username (str): The username to authenticate.
-    password (str): The password to authenticate.
-    """
+    """ Authenticate the user. """
     login_df = st.session_state.df_users
     login_df['username'] = login_df['username'].astype(str)
 
     if username in login_df['username'].values:
         stored_hashed_password = login_df.loc[login_df['username'] == username, 'password'].values[0]
-        stored_hashed_password_bytes = binascii.unhexlify(stored_hashed_password)  # Convert hex to bytes
+        stored_hashed_password_bytes = binascii.unhexlify(stored_hashed_password)
         
-        # Check the input password
         if bcrypt.checkpw(password.encode('utf8'), stored_hashed_password_bytes): 
             st.session_state['authentication'] = True
             st.session_state['username'] = username
-            st.success('Login successful')
+            st.success(translate_text('Login successful', st.session_state['target_language']))
             st.experimental_rerun()
         else:
-            st.error('Incorrect password')
+            st.error(translate_text('Incorrect password', st.session_state['target_language']))
     else:
-        st.error('Username not found')
+            st.error(translate_text('Username not found', st.session_state['target_language']))
 
+# Page switching function
 def switch_page(page_name):
-    st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
-    time.sleep(3)
+    st.success(translate_text(f"Redirecting to {page_name.replace('_', ' ')} page...", st.session_state['target_language']))
     st.experimental_set_query_params(page=page_name)
+    time.sleep(3)
     st.experimental_rerun()
+
+def main():
+    init_github()
+    init_credentials()
+
+    if 'authentication' not in st.session_state:
+        st.session_state['authentication'] = False
+
+    # Language selection
+    if 'target_language' not in st.session_state:
+        languages = {
+            "English": "en",
+            "German": "de",
+            "Spanish": "es",
+            "French": "fr",
+            "Chinese": "zh-cn"
+        }
+        selected_language = st.selectbox("Choose your language", list(languages.keys()), index=0)
+        st.session_state['target_language'] = languages[selected_language]
+    else:
+        st.write(translate_text("Language: ", st.session_state['target_language']) + st.session_state['target_language'])
+
+    if not st.session_state['authentication']:
+        options = st.sidebar.selectbox(translate_text("Select a page", st.session_state['target_language']), ["Login", "Register"])
+        if options == "Login":
+            login_page()
+        elif options == "Register":
+            register_page()
+    else:
+        st.sidebar.write(translate_text("Logged in as", st.session_state['target_language']) + f" {st.session_state['username']}")
+        main_page()
+        anxiety_assessment()
+        if st.sidebar.button(translate_text("Logout", st.session_state['target_language'])):
+            st.session_state['authentication'] = False
+            st.session_state.pop('username', None)
+            switch_page("main.py")
 
 if __name__ == "__main__":
     main()
