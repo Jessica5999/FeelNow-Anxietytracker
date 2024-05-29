98411236ddc74013307768a658882b274e7e7771
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 10:03:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ translated_text = translate_text(original_text, target_language)
 st.write(translated_text)
 
 col1, col2 = st.columns([0.8, 0.2])
- with col2:
+with col2:
     if st.button("Login/Register"):
     st.switch_page("pages/1_login.py")
 
