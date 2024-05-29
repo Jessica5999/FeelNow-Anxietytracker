2f0df3a4378e9764f66a3e3d100dea51c24b6e9c
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 11:02:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,20 +48,17 @@ def main_page():
     selected_language = st.selectbox("Choose your language", list(languages.keys()), index=0)
     target_language = languages[selected_language]
 
-    original_text = """
-    Welcome to FeelNow, your anxiety attack journal.
-    This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
-    and feelings during anxiety attacks.
-    
-    ## What is FeelNow
-    FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
-    
-    ## What can the App do
-    The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies takeing notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
-    
-    ## How do I use it
-    You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
-    """
+    original_text = (
+        "Welcome to FeelNow, your anxiety attack journal. "
+        "This app helps you track and manage your anxiety by providing a platform to journal your thoughts "
+        "and feelings during anxiety attacks.\n\n"
+        "## What is FeelNow\n"
+        "FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.\n\n"
+        "## What can the App do\n"
+        "The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies taking notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.\n\n"
+        "## How do I use it\n"
+        "You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.\n"
+    )
 
     # Translate the text
     translated_text = translate_text(original_text, target_language)
