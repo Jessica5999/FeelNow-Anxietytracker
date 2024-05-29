a043431f189c1d4865f868a3035f42b01787e529
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 10:27:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from github_contents import GithubContents
 from deep_translator import GoogleTranslator  # Import the GoogleTranslator class from the deep_translator library
+import time  # Ensure that you import the time module
 
 github = GithubContents(
     st.secrets["github"]["owner"],
@@ -30,12 +31,11 @@ def translate_text(text, target_language):
     return translation
 
 # Function to display the main page
-
 def main_page():
     st.image("Logo.jpeg", width=600)
     st.subheader("Anxiety Tracker Journal")
 
-     # Supported languages
+    # Supported languages
     languages = {
         "English": "en",
         "German": "de",
@@ -43,29 +43,30 @@ def main_page():
         "French": "fr",
         "Chinese": "zh-cn"
     }
-orginal_text = """
-        Welcome to FeelNow, your anxiety attack journal.
-        This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
-        and feelings during anxiety attacks.
-        
-        ## What is FeelNow
-        FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
-        
-        ## What can the App do
-        The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies takeing notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
-        
-        ## How do I use it
-        You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
-        """
+    
+    original_text = """
+    Welcome to FeelNow, your anxiety attack journal.
+    This app helps you track and manage your anxiety by providing a platform to journal your thoughts 
+    and feelings during anxiety attacks.
+    
+    ## What is FeelNow
+    FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
+    
+    ## What can the App do
+    The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies takeing notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
+    
+    ## How do I use it
+    You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
+    """
 
-# Translate the text
-translated_text = translate_text(original_text, target_language)
-st.write(translated_text)
+    target_language = "de"  # You can set the target language as per your requirement or user input
+    translated_text = translate_text(original_text, target_language)
+    st.write(translated_text)
 
-col1, col2 = st.columns([0.8, 0.2])
-with col2:
- if st.button("Login/Register"):
-    st.switch_page("pages/1_login.py")
+    col1, col2 = st.columns([0.8, 0.2])
+    with col2:
+        if st.button("Login/Register"):
+            st.switch_page("pages/1_login.py")
 
 def switch_page(page_name):
     st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
