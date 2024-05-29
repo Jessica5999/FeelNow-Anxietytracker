aec77d20da9db18747a9608e9c3fdcaa8b96e34f
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-05-29 10:44:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,15 +57,17 @@ def main_page():
     FeelNow is an app with which you can easily assess and monitor an acute panic attack. It is just like a diary and helps you to keep an eye on your mental health.
     
     ## What can the App do
-    The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies taking notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
+    The app is supposed to help you write down important parts of a panic attack or even simply for your anxiety. It simplifies takeing notes while feeling distressed by having the option to just choose how you're feeling instead of having to write your feelings down yourself.
     
     ## How do I use it
     You can create your own login by registering. You will then have a list of important points to assess during an acute attack, such as symptoms, possible triggers, who helped you at that moment or how strongly you felt them. If you do not feel like you're having a panic attack but you do feel anxious, you can do the same in the simpler version.
     """
 
-    # Translate the text
-    translated_text = translate_text(original_text, target_language)
-    st.write(translated_text)
+    if target_language == "de":
+        translated_text = translate_text(original_text, target_language)
+        st.write(translated_text)
+    else:
+        st.code(original_text, language='text')
 
     col1, col2 = st.columns([0.8, 0.2])
     with col2:
