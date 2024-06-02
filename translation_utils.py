49589d6f64dfb192ee5c4447ff6768d871ee7c54
Jessica5999 <162372341+Jessica5999@users.noreply.github.com>
2024-06-02 14:47:35 +0200

    Delete translation_utils.py

--- translation_utils.py
+++
@@ -1,7 +0,0 @@
-# translation_utils.py
-from deep_translator import GoogleTranslator
-
-def translate_text(text, target_language):
-    translator = GoogleTranslator(target=target_language)  # Initialize the GoogleTranslator object
-    translation = translator.translate(text)  # Translate the text
-    return translation
